6339a2ffcc06cdd63f6f707e211dafa163ede431
amandahharman <ahh6007@uncw.edu>
2016-06-08 22:21:54 -0400

    Add icons for weather, which change based in the icon string given back from the weather api

---
+++ Weather/Weather.swift
@@ -0,0 +1,14 @@
+//
+//  Weather.swift
+//  Weather
+//
+//  Created by Amanda Harman on 6/8/16.
+//  Copyright © 2016 Amanda Harman. All rights reserved.
+//
+
+import Foundation
+
+struct Weather {
+    var temp: Int
+    var icon: String = "none"
+}
